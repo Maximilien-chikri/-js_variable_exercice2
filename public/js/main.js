6145c0af4b3e73a499c0b40bef567453f4f64125
Maximilien <maximilienchikri@gmail.com>
2020-12-28 14:14:21 +0100

    fin exo

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// contentation
 let maPhrase = "j'aime les tartines et j'en mange";
 console.log(maPhrase);
 let nbrTartine = "4 par jours";
@@ -6,12 +7,20 @@ let third = 5;
 let fourth = 35;
 console.log(third + fourth);
 console.log(maPhrase + " " + third);
+// réafectation
 let salut = "salut tout le monde";
 console.log(salut);
 let bye = "Aurevoir !";
 console.log(bye);
 salut = bye
 console.log(salut);
-
+// égalités,différences et booleans
+let number1 = 4;
+let number2 = "6";
+console.log(number1 < number2);
+console.log(number1 == number2);
+console.log(number1 != number2);
+console.log(number1 === number2);
+console.log(number1 !== number2);
 
 
