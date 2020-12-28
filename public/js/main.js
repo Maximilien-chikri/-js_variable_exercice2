013e5c273a922f6193f0441909a5de628f80e414
Maximilien <maximilienchikri@gmail.com>
2020-12-28 12:43:48 +0100

    contentation et réafectation

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+let maPhrase = "j'aime les tartines et j'en mange";
+console.log(maPhrase);
+let nbrTartine = "4 par jours";
+console.log(maPhrase + " " + nbrTartine);
+let third = 5;
+let fourth = 35;
+console.log(third + fourth);
+console.log(maPhrase + " " + third);
+let salut = "salut tout le monde";
+console.log(salut);
+let bye = "Aurevoir !";
+console.log(bye);
+salut = bye
+console.log(salut);
+
+
+
